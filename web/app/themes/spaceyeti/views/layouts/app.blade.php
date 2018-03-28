5ebbbe5a838119446f820453e72f61c4bdd9def0
Christian Gellweiler <cgellweiler@gmail.com>
2018-03-28 22:15:32 +0200

    Fix flex layout for equal height blog posts

--- web/app/themes/spaceyeti/views/layouts/app.blade.php
+++ web/app/themes/spaceyeti/views/layouts/app.blade.php
@@ -35,7 +35,10 @@
 
   <!-- ganz oben -->
   <section class="container">
-    <div class="card post-module columns is-multiline">
+  <div class="columns">
+      <div class="column is-12">
+
+    <div class="post-module">
       <div class="thumbnail column is-two-thirds-widescreen is-full-desktop is-full-mobile is-full-tablet">
           <img class="image" src="https://www.littlebluebag.de/app/uploads/2017/03/4.7.10-Langzeitbelichtung-bei-Tag_NYC_littlebluebag_0005.jpg"/>
           <div class="date">
@@ -46,21 +49,6 @@
       </div>
       
       <div class="post-content column is-one-third-widescreen is-full-desktop is-full-mobile is-full-tablet">
-        <!-- <div class="card__share">
-          <div class="card__social">
-            <a class="share-icon facebook" href="#">
-                <span class="fa fa-facebook"></span>
-            </a>
-            <a class="share-icon twitter" href="#">
-                <span class="fa fa-twitter"></span>
-            </a>
-            <a class="share-icon pinterest" href="#">
-                <span class="fa fa-pinterest"></span>
-            </a>
-          </div>
-          <a id="share" class="share-toggle share-icon" href="#"></a>
-        </div> -->
-
         <h1 class="title">Deine Fotos nach der Reise Lorem Ipsum</h1>
         <h2 class="sub_title">Kostenlose Foto CheckListe zum Download</h2>
         <p class="description">Es ist Herbst. Regen, Nebel, Sonne, bunter Blätter… es gibt jede Menge toller herbstlicher Fotomotive, daher gibt es für dich eine kostenlose Herbst Foto Checkliste für den Herbst von uns!
@@ -79,95 +67,45 @@
         </div>
       </div>
     </div>
+  </div></div>
   </section>
 
 
-  <!-- ganz oben -->
-  <!-- <section class="container">
-    <div class="card post-module">
-      <div class="thumbnail">
-          <div class="date">
-              <div class="day">27</div>
-              <div class="month">Mar</div>
-          </div>
-          <img src="https://www.littlebluebag.de/app/uploads/2017/03/4.7.10-Langzeitbelichtung-bei-Tag_NYC_littlebluebag_0005.jpg"/>
-      </div>
-      <div class="category">Fotografie-Tipps</div>
-      <div class="post-content">
-        <div class="card__share">
-          <div class="card__social">
-            <a class="share-icon facebook" href="#">
-                <span class="fa fa-facebook"></span>
-            </a>
-            <a class="share-icon twitter" href="#">
-                <span class="fa fa-twitter"></span>
-            </a>
-            <a class="share-icon pinterest" href="#">
-                <span class="fa fa-pinterest"></span>
-            </a>
-          </div>
-          <a id="share" class="share-toggle share-icon" href="#"></a>
-        </div>
+  <!-- 3 artikel nebeneinander -->
 
-        <h1 class="title">Deine Fotos nach der Reise Lorem Ipsum</h1>
-        <h2 class="sub_title">Kostenlose Foto CheckListe zum Download</h2>
-        <p class="description">Es ist Herbst. Regen, Nebel, Sonne, bunter Blätter… es gibt jede Menge toller herbstlicher Fotomotive, daher gibt es für dich eine kostenlose Herbst Foto Checkliste für den Herbst von uns!
-
-        </p>
-        <i class="fa fa-share-alt"></i>
-        <i class="fa fa-share"></i>
-
-
-        <div class="post-meta">
-          <span class="timestamp"><i class="fa fa-clock-">o</i> 6 mins ago</span>
-          <span class="comments">
-            <i class="fa fa-comments"></i>
-            <a href="#"> 39 comments</a>
-          </span>
-        </div>
-      </div>
-    </div>
-  </section> -->
-
-  <section class="container" >
-    <div class="columns" >
-      <div class="column is-4" style="align-items: stretch;display: flex;">
-        <section class="articles">
-
-          <div class="card post-module">
+  <section class="container">
+    <div class="columns">
+      <div class="column is-4 flexon">
+          <div class="post-module flexlayout">
             <div class="thumbnail">
                 <div class="date">
                     <div class="day">27</div>
                     <div class="month">Mar</div>
                 </div>
-                <!-- <img src="https://www.littlebluebag.de/app/uploads/2017/03/4.7.10-Langzeitbelichtung-bei-Tag_NYC_littlebluebag_0005.jpg"/> -->
-                <img src="https://www.littlebluebag.de/app/uploads/2017/10/Nachtfotografie-Workshop-Mannheim_LittleBlueBag_Reiseblog-5.jpg"/>
+                <img class="image" src="https://www.littlebluebag.de/app/uploads/2018/03/Schweden-mit-Anja-17.-23.10.17-428.jpg"/>
             </div>
             <div class="category">Fotografie-Tipps</div>
             <div class="post-content">
               <div class="card__share">
                 <div class="card__social">
                   <a class="share-icon facebook" href="#">
-                      <span class="fa fa-facebook"></span>
+                    <span class="fa fa-facebook"></span>
                   </a>
                   <a class="share-icon twitter" href="#">
-                      <span class="fa fa-twitter"></span>
+                    <span class="fa fa-twitter"></span>
                   </a>
                   <a class="share-icon pinterest" href="#">
-                      <span class="fa fa-pinterest"></span>
+                    <span class="fa fa-pinterest"></span>
                   </a>
                 </div>
                 <a id="share" class="share-toggle share-icon" href="#"></a>
               </div>
 
-                <h1 class="title">Deine Fotos nach der Reise Lorem Ipsum</h1>
+              <h1 class="title">Deine Fotos nach der Reise Lorem Ipsum</h1>
               <h2 class="sub_title">Kostenlose Foto CheckListe zum Download</h2>
-              <p class="description">Es ist Herbst. Regen, Nebel, Sonne, bunter Blätter… es gibt jede Menge toller herbstlicher Fotomotive, daher gibt es für dich eine kostenlose Herbst Foto Checkliste für den Herbst von uns!
-
-              </p>
+              <p class="description">Es ist Herbst. Regen, Nebel, Sonne, bunter Blätter… es gibt jede Menge toller herbstlicher Fotomotive, daher gibt es für dich eine kostenlose Herbst Foto Checkliste für den Herbst von uns!</p>
               <i class="fa fa-share-alt"></i>
               <i class="fa fa-share"></i>
-
 
               <div class="post-meta">
                 <span class="timestamp"><i class="fa fa-clock-">o</i> 6 mins ago</span>
@@ -178,118 +116,100 @@
               </div>
             </div>
           </div>
-
-        </section>
       </div>
 
       <!-- side bar -->
-     <div class="column is-4" style="align-items: stretch;display: flex;">
-
-        <!-- <div class="post-module">
-            <div class="thumbnail">
-                <div class="date">
-                    <div class="day">27</div>
-                    <div class="month">Mar</div>
-                </div>
-                <img src="https://www.littlebluebag.de/app/uploads/2017/10/Nachtfotografie-Workshop-Mannheim_LittleBlueBag_Reiseblog-5.jpg"/>
+     <div class="column is-4 flexon"> 
+        <div class="card post-module flexlayout">
+          <div class="thumbnail">
+            <div class="date">
+              <div class="day">27</div>
+              <div class="month">Mar</div>
             </div>
-            <div class="post-content">
-
-                <div class="card__share">
-                    <div class="card__social">
-                        <a class="share-icon facebook" href="#">
-                            <span class="fa fa-facebook"></span>
-                        </a>
-                        <a class="share-icon twitter" href="#">
-                            <span class="fa fa-twitter"></span>
-                        </a>
-                        <a class="share-icon pinterest" href="#">
-                            <span class="fa fa-pinterest"></span>
-                        </a>
-                    </div>
-    
-                    <a id="share" class="share-toggle share-icon" href="#"></a>
-                </div>
-    
-                <div class="category">Fotografie-Tipps</div>
-                <h1 class="title">Deine Fotos nach der Reise Lorem Ipsum</h1>
-                <h2 class="sub_title">Kostenlose Foto CheckListe zum Download</h2>
-                <p class="description">Es ist Herbst. Regen, Nebel, Sonne, bunter Blätter… es gibt jede Menge toller herbstlicher Fotomotive, daher gibt es für dich eine kostenlose Herbst Foto Checkliste für den Herbst von uns!</p>
-                <div class="post-meta">
-                    <span class="timestamp">
-                        <i class="fa fa-clock-">o</i> 6 mins ago</span>
-                    <span class="comments">
-                        <i class="fa fa-comments"></i>
-                        <a href="#"> 39 comments</a>
-                    </span>
-                </div>
-            </div>
-</div> -->
-
-      <section class="articles">
-          <div class="card post-module">
-            <div class="thumbnail">
-                <div class="date">
-                    <div class="day">27</div>
-                    <div class="month">Mar</div>
-                </div>
-                <img src="https://www.littlebluebag.de/app/uploads/2017/03/4.7.10-Langzeitbelichtung-bei-Tag_NYC_littlebluebag_0005.jpg"/>
-                <!-- <img src="https://www.littlebluebag.de/app/uploads/2017/10/Nachtfotografie-Workshop-Mannheim_LittleBlueBag_Reiseblog-5.jpg"/>-->
-            </div>
-            <div class="category">Fotografie-Tipps</div>
-            <div class="post-content">
-              <div class="card__share">
-                <div class="card__social">
-                  <a class="share-icon facebook" href="#">
-                      <span class="fa fa-facebook"></span>
-                  </a>
-                  <a class="share-icon twitter" href="#">
-                      <span class="fa fa-twitter"></span>
-                  </a>
-                  <a class="share-icon pinterest" href="#">
-                      <span class="fa fa-pinterest"></span>
-                  </a>
-                </div>
-                <a id="share" class="share-toggle share-icon" href="#"></a>
+            <img class="image" src="https://www.littlebluebag.de/app/uploads/2017/03/4.7.10-Langzeitbelichtung-bei-Tag_NYC_littlebluebag_0005.jpg"/>
+            <!-- <img src="https://www.littlebluebag.de/app/uploads/2017/10/Nachtfotografie-Workshop-Mannheim_LittleBlueBag_Reiseblog-5.jpg"/>-->
+          </div>
+          <div class="category">Fotografie-Tipps</div>
+          <div class="post-content">
+            <div class="card__share">
+              <div class="card__social">
+                <a class="share-icon facebook" href="#">
+                  <span class="fa fa-facebook"></span>
+                </a>
+                <a class="share-icon twitter" href="#">
+                  <span class="fa fa-twitter"></span>
+                </a>
+                <a class="share-icon pinterest" href="#">
+                  <span class="fa fa-pinterest"></span>
+                </a>
               </div>
+              <a id="share" class="share-toggle share-icon" href="#"></a>
+            </div>
 
-                <h1 class="title">Deine Fotos nach der Reise Lorem Ipsum</h1>
-              <h2 class="sub_title">Kostenlose Foto CheckListe zum Download</h2>
-              <p class="description">Es ist Herbst. Regen, Nebel, Sonne, bunter Blätter… es gibt jede Menge toller herbstlicher Fotomotive, daher gibt es für dich eine kostenlose Herbst Foto Checkliste für den Herbst von uns!
+            <h1 class="title">Deine Fotos nach der Reise Lorem Ipsum</h1>
+            <h2 class="sub_title">Kostenlose Foto CheckListe zum Download</h2>
+            <p class="description">Es ist Herbst. Regen, Nebel, Sonne, bunter Blätter… es gibt jede Menge toller herbstlicher Fotomotive, daher gibt es für dich eine kostenlose Herbst Foto Checkliste für den Herbst von uns!</p>
+            <i class="fa fa-share-alt"></i>
+            <i class="fa fa-share"></i>
 
-              </p>
-              <i class="fa fa-share-alt"></i>
-              <i class="fa fa-share"></i>
-
-
-              <div class="post-meta">
-                <span class="timestamp"><i class="fa fa-clock-">o</i> 6 mins ago</span>
-                <span class="comments">
-                  <i class="fa fa-comments"></i>
-                  <a href="#"> 39 comments</a>
-                </span>
-              </div>
+            <div class="post-meta">
+              <span class="timestamp"><i class="fa fa-clock-">o</i> 6 mins ago</span>
+              <span class="comments">
+                <i class="fa fa-comments"></i>
+                <a href="#"> 39 comments</a>
+              </span>
             </div>
           </div>
-</section>
-
-
-
-
-      </div>
-    </div
-  </section>
-
-  <!-- <section class="section">
-    <div class="container">
-      <h1 class="title">
-        Hello World
-      </h1>
-      <p class="subtitle">
-        My first website with <strong>Bulma</strong>!
-      </p>
+        </div>
     </div>
-  </section> -->
+
+      <!-- side bar -->
+      <div class="column is-4 flexon"> 
+        <div class="card post-module flexlayout">
+          <div class="thumbnail">
+            <div class="date">
+              <div class="day">27</div>
+              <div class="month">Mar</div>
+            </div>
+            <!-- <img src="https://www.littlebluebag.de/app/uploads/2017/03/4.7.10-Langzeitbelichtung-bei-Tag_NYC_littlebluebag_0005.jpg"/> -->
+            <img src="https://www.littlebluebag.de/app/uploads/2017/10/Nachtfotografie-Workshop-Mannheim_LittleBlueBag_Reiseblog-5.jpg"/>
+            
+          </div>
+          <div class="category">Fotografie-Tipps</div>
+          <div class="post-content">
+            <div class="card__share">
+              <div class="card__social">
+                <a class="share-icon facebook" href="#">
+                  <span class="fa fa-facebook"></span>
+                </a>
+                <a class="share-icon twitter" href="#">
+                  <span class="fa fa-twitter"></span>
+                </a>
+                <a class="share-icon pinterest" href="#">
+                  <span class="fa fa-pinterest"></span>
+                </a>
+              </div>
+              <a id="share" class="share-toggle share-icon" href="#"></a>
+            </div>
+
+            <h1 class="title">Deine Fotos nach der Reise Lorem Ipsum</h1>
+            <h2 class="sub_title">Kostenlose Foto CheckListe zum Download</h2>
+            <p class="description">Es ist Herbst. Regen, Nebel, Sonne, bunter Blätter… es gibt jede Menge toller herbstlicher Fotomotive, daher gibt es für dich eine kostenlose Herbst Foto Checkliste für den Herbst von uns!</p>
+            <i class="fa fa-share-alt"></i>
+            <i class="fa fa-share"></i>
+
+            <div class="post-meta">
+              <span class="timestamp"><i class="fa fa-clock-">o</i> 6 mins ago</span>
+              <span class="comments">
+                <i class="fa fa-comments"></i>
+                <a href="#"> 39 comments</a>
+              </span>
+            </div>
+          </div>
+        </div>
+    </div>
+
+</section>
 
   @php(wp_footer())
     <noscript id="deferred-styles">
